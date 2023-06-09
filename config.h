3d2e9b373dfff1d74017c0442dfa626571667e3e
Kyle Huggins <kylehuggins2.0@gmail.com>
2023-06-09 08:47:22 -0500

    fix: remove EE_HANDS

--- config.h
+++ config.h
@@ -4,24 +4,23 @@
 #define NO_MUSIC_MODE
 
 #ifdef SPLIT_KEYBOARD
-    #define EE_HANDS
-    #define MASTER_RIGHT
+#define MASTER_RIGHT
 #endif
 
 #ifdef MOUSEKEY_ENABLE
-    #define PIMORONI
-    #define POINTING_DEVICE_ROTATION_90
-    #ifdef TRACKBALL_RIGHT
-        #define POINTING_DEVICE_INVERT_X
-        #define POINTING_DEVICE_INVERT_Y
-    #endif
+#define PIMORONI
+#define POINTING_DEVICE_ROTATION_90
+#ifdef TRACKBALL_RIGHT
+#define POINTING_DEVICE_INVERT_X
+#define POINTING_DEVICE_INVERT_Y
+#endif
 
-    # define MOUSEKEY_INTERVAL 15 // Time between cursor movements in milliseconds
-    # define MOUSEKEY_MAX_SPEED 9
-    # define MOUSEKEY_MOVE_DELTA 10 // Step size for acceleration
-    # define MOUSEKEY_TIME_TO_MAX 80
-    # define MOUSEKEY_WHEEL_DELAY 16
-    # define MOUSEKEY_WHEEL_INTERVAL 30
-    # define MOUSEKEY_WHEEL_MAX_SPEED 10
-    # define MOUSEKEY_WHEEL_TIME_TO_MAX 95
+#define MOUSEKEY_INTERVAL 15 // Time between cursor movements in milliseconds
+#define MOUSEKEY_MAX_SPEED 9
+#define MOUSEKEY_MOVE_DELTA 10 // Step size for acceleration
+#define MOUSEKEY_TIME_TO_MAX 80
+#define MOUSEKEY_WHEEL_DELAY 16
+#define MOUSEKEY_WHEEL_INTERVAL 30
+#define MOUSEKEY_WHEEL_MAX_SPEED 10
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 95
 #endif
